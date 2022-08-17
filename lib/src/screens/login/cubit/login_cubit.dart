a22e709b8036385394226dfea20d4183d34d1dc4
Ammar Ayoubi <59621670+ammarayoubi@users.noreply.github.com>
2022-08-17 19:30:41 +0500

    added interface.

--- lib/src/screens/login/cubit/login_cubit.dart
+++ lib/src/screens/login/cubit/login_cubit.dart
@@ -1,8 +1,9 @@
+import 'package:base_project/src/screens/login/login_interface.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
-class LoginCubit extends Cubit<LoginState> {
+class LoginCubit extends Cubit<LoginState> implements LoginInterface{
   LoginCubit() : super(LoginInitial());
 }
--- lib/src/screens/splash/cubit/splash_cubit.dart
+++ lib/src/screens/splash/cubit/splash_cubit.dart
@@ -1,8 +1,9 @@
+import 'package:base_project/src/screens/splash/splash_interface.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'splash_state.dart';
 
-class SplashCubit extends Cubit<SplashState> {
+class SplashCubit extends Cubit<SplashState> implements SplashInterface{
   SplashCubit() : super(SplashInitial());
 }
