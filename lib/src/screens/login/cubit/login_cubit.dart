4c8d954bea6fc33371137105b3002172b9e0563d
Adil Ahmed <adil.rangila@gmail.com>
2022-08-18 06:57:29 -0700

    login page removed

--- lib/src/screens/login/cubit/login_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:base_project/src/screens/login/login_interface.dart';
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'login_state.dart';
-
-class LoginCubit extends Cubit<LoginState> implements LoginInterface{
-  LoginCubit() : super(LoginInitial());
-}
